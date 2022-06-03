44c66aee3d21bc4b5e95dd5cdad5f994040e80eb
joaogfarias@hotmail.com <joaogfarias@hotmail.com>
2022-06-03 11:58:00 +0000

    feat: Lab#2 - variables and data types completed

--- Just-Enough-Python-For-Spark/Labs/JEP 02L - Variables and Data Types Lab.py
+++ Just-Enough-Python-For-Spark/Labs/JEP 02L - Variables and Data Types Lab.py
@@ -25,7 +25,7 @@
 # COMMAND ----------
 
 # TODO
-FILL_IN = FILL_IN # Create the variable conversion_rate and assign it the value of 1.18
+conversion_rate = 1.18 # Create the variable conversion_rate and assign it the value of 1.18
 
 # COMMAND ----------
 
@@ -36,7 +36,7 @@ FILL_IN = FILL_IN # Create the variable conversion_rate and assign it the value 
 
 # TODO
 # Assert that your variable is of the correct type
-assert FILL_IN == FILL_IN, FILL_IN
+assert type(conversion_rate) == float, f"Conversion rate was expected as float, but it was found as {type(conversion_rate)}"
 
 # COMMAND ----------
 
@@ -50,8 +50,8 @@ assert FILL_IN == FILL_IN, FILL_IN
 # COMMAND ----------
 
 # TODO
-FILL_IN = FILL_IN # Create the variable euro_amount and assign it the value of 567
-FILL_IN = FILL_IN # Assign the result of the computation to usd_amount
+euro_amount = 567 # Create the variable euro_amount and assign it the value of 567
+usd_amount = euro_amount * conversion_rate # Assign the result of the computation to usd_amount
 
 # Test your solution
 assert usd_amount == 669.06, "Incorrect amount."
@@ -68,7 +68,7 @@ assert usd_amount == 669.06, "Incorrect amount."
 # COMMAND ----------
 
 # TODO
-print(FILL_IN)
+print(f"{euro_amount} Euris is equal to ${usd_amount} USD")
 
 # COMMAND ----------
 
