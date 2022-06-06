1b4fba24f3d7da9e77fee630ccfd29d2ba9bc208
joaogfarias@hotmail.com <joaogfarias@hotmail.com>
2022-06-06 14:42:06 +0000

    feat: Lab#3 - conditions and loops completed

--- Just-Enough-Python-For-Spark/Labs/JEP 03L - Fizz Buzz Lab.py
+++ Just-Enough-Python-For-Spark/Labs/JEP 03L - Fizz Buzz Lab.py
@@ -32,10 +32,15 @@
 
 # COMMAND ----------
 
-# TODO
-
-for FILL_IN
-  if FILL_IN
+for number in range(1, 101):
+    if (number % 3 == 0) and (number % 5 == 0):
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
 # COMMAND ----------
 
